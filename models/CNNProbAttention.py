b80e6f81acdf57811836a9f0f0c2af5f30f4a402
OliverZ11x <zhaoshiyi2000@qq.com>
2022-11-24 12:42:24 +0800

    [update]add my models

---
+++ models/CNNProbAttention.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from utils.masking import TriangularCausalMask, ProbMask
+from layers.Transformer_EncDec import Decoder, DecoderLayer, Encoder, EncoderLayer, ConvLayer
+from layers.SelfAttention_Family import FullAttention, ProbAttention, AttentionLayer
+from layers.Embed import DataEmbedding, DataEmbedding_onlyvalue
+import numpy as np
+
+
+class Model(nn.Module):
+    def __init__(self, configs):
+        super(Model, self).__init__()
+        self.pred_len = configs.pred_len
+        self.output_attention = configs.output_attention
+        
+        self.conv1 = nn.Conv1d(in_channels=251, out_channels=500, kernel_size=1)
+        
+        # Embedding
+        self.enc_embedding = DataEmbedding_onlyvalue(configs.enc_in, configs.d_model, configs.embed, configs.freq,
+                                                     configs.dropout)
+
+        # Encoder
+        self.encoder = Encoder(
+            [
+                EncoderLayer(
+                    AttentionLayer(
+                        ProbAttention(False, configs.factor, attention_dropout=configs.dropout,
+                                      output_attention=configs.output_attention),
+                        configs.d_model, configs.n_heads),
+                    configs.d_model,
+                    configs.d_ff,
+                    dropout=configs.dropout,
+                    activation=configs.activation
+                ) for l in range(configs.e_layers)
+            ],
+            [
+                ConvLayer(
+                    configs.d_model
+                ) for l in range(configs.e_layers - 1)
+            ]if configs.distil else None,
+            norm_layer=torch.nn.LayerNorm(configs.d_model),
+            projection=nn.Linear(configs.d_model, configs.c_out, bias=True)
+        )
+
+    def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec,
+                enc_self_mask=None, dec_self_mask=None, dec_enc_mask=None):
+
+        enc_out = self.enc_embedding(x_enc, x_mark_enc)
+        enc_out, attns = self.encoder(enc_out, attn_mask=enc_self_mask)
+        
+        enc_out = self.conv1(enc_out)
+        
+        if self.output_attention:
+            return enc_out[:, -self.pred_len:, :], attns
+        else:
+            return enc_out[:, -self.pred_len:, :]
